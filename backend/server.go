47d43e1827797614e1442c27ab98039f85cdc77b
Nihill <dimcoj5@gmail.com>
2022-08-01 23:09:28 +0300

    [add] (server.go) func getData() /from file/

--- backend/server.go
+++ backend/server.go
@@ -1,7 +1,9 @@
 package backend
 
 import (
+	"io"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -23,5 +25,24 @@ func ensureDataFileExists() {
 		}
 	} else if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func getData(w http.ResponseWriter, r *http.Request) {
+	dataFile, err := os.Open(dataFilePath)
+	if err != nil {
+		log.Fatal("file open on get", err.Error())
+		w.WriteHeader(500)
+		io.WriteString(w, "error reading file")
+		return
+	}
+
+	defer dataFile.Close()
+
+	_, err = io.Copy(dataFile, r.Body)
+	if err != nil {
+		log.Print("copy from request: ", err.Error())
+		w.WriteHeader(500)
+		return
 	}
 }
