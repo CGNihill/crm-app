80aea7a8bf701d84d00bbefe1e99a505103fdae9
Nihill <dimcoj5@gmail.com>
2022-08-02 03:34:04 +0300

    fin

--- backend/server.go
+++ backend/server.go
@@ -10,7 +10,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-const dataFilePath = "./data/crm-app-out.txt"
+/* output file */
+const dataFilePath = "./data/crmappout.txt"
 
 func ensureDataFileExists() {
 	_, err := os.Stat(dataFilePath)
@@ -21,8 +22,8 @@ func ensureDataFileExists() {
 		}
 
 		defer dataFile.Close()
-		_, err = dataFile.Write([]byte("[]"))
 
+		_, err = dataFile.Write([]byte("[]"))
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -31,6 +32,7 @@ func ensureDataFileExists() {
 	}
 }
 
+/* get data from file */
 func getData(w http.ResponseWriter, r *http.Request) {
 	dataFile, err := os.Open(dataFilePath)
 	if err != nil {
@@ -44,43 +46,47 @@ func getData(w http.ResponseWriter, r *http.Request) {
 
 	_, err = io.Copy(dataFile, r.Body)
 	if err != nil {
-		log.Print("copy from request: ", err.Error())
+		log.Println("copy from request: ", err.Error())
 		w.WriteHeader(500)
 		return
 	}
 }
 
+/* post data */
 func postData(w http.ResponseWriter, r *http.Request) {
 	dataFile, err := os.OpenFile(dataFilePath, os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
-		log.Println("file open on post func :", err.Error())
+		log.Println("file open on post func", err.Error())
 		w.WriteHeader(500)
-		io.WriteString(w, "error reading file")
+		io.WriteString(w, "error reading the file!!")
 		return
 	}
 
 	defer dataFile.Close()
 
-	_, err = io.Copy(w, dataFile)
+	_, err = io.Copy(dataFile, r.Body)
 	if err != nil {
-		log.Println("copy from request :", err.Error())
+		log.Println("copy from request: ", err.Error())
 		w.WriteHeader(500)
 		return
 	}
 }
 
+/* handle the homepage */
 func homePage(w http.ResponseWriter, r *http.Request) {
 	indexFile, err := os.Open("./static/index.html")
 	if err != nil {
-		io.WriteString(w, "error reading html file")
+		io.WriteString(w, "error reading html file. make sure you have it!!")
 		return
 	}
 
 	defer indexFile.Close()
 
 	io.Copy(w, indexFile)
+
 }
 
+/* start the server */
 func Start() {
 	ensureDataFileExists()
 
@@ -88,7 +94,7 @@ func Start() {
 
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "127.0.0.1:8000",
+		Addr:         "127.0.0.1:8080",
 		WriteTimeout: 60 * time.Second,
 		ReadTimeout:  60 * time.Second,
 	}
@@ -96,7 +102,7 @@ func Start() {
 	r.HandleFunc("/", homePage)
 	r.Methods("GET").Path("/data").HandlerFunc(getData)
 	r.Methods("POST").Path("/data").HandlerFunc(postData)
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	log.Printf("Server started on %s\n", srv.Addr)
 	log.Fatal(srv.ListenAndServe())
